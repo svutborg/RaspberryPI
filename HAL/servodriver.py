2fe76b2dc2d6609d21ab9b16effcea0021498fcd
svutborg <svu@ucn.dk>
2020-09-18 18:07:07 +0200

    Added servo files

---
+++ HAL/servodriver.py
@@ -0,0 +1,53 @@
+if __name__ == "__main__":
+	from i2c import pca9685
+	import time
+else:
+	from HAL.i2c import pca9685
+
+
+class ServoDriver:
+	sloppy_mode = 0  # lets the precise frequency float
+	strict_mode = 1  # Keeps the frequency precise
+
+	def __init__(self, frequency=5, mode=sloppy_mode):
+		self.frequency = frequency
+		self.mode = mode
+		self.controller = pca9685.PCA9685(0x40)
+		#self.led_off()
+
+	# def led_off(self):
+	# 	self.controller.write_to_register(self.controller.LEDX_YY_Z(0, 0, 1), 1 << 4)
+	# 	self.controller.write_to_register(self.controller.LEDX_YY_Z(0, 1, 1), 0)
+	# 	self.controller.write_to_register(self.controller.LEDX_YY_Z(1, 0, 1), 1 << 4)
+	# 	self.controller.write_to_register(self.controller.LEDX_YY_Z(1, 1, 1), 0)
+	# 	self.controller.write_to_register(self.controller.LEDX_YY_Z(2, 0, 1), 1 << 4)
+	# 	self.controller.write_to_register(self.controller.LEDX_YY_Z(2, 1, 1), 0)
+	#
+	# def led_on(self):
+	# 	self._set_duty((255>>3)*self.intensity, 2)
+	# 	self._set_duty((255>>3)*self.intensity, 1)
+	# 	self._set_duty((255>>3)*self.intensity, 0)
+	#
+	# def LED_RGB(self, r, g, b):
+	# 	assert 0x00 <= r <= 0xFF
+	# 	assert 0x00 <= g <= 0xFF
+	# 	assert 0x00 <= b <= 0xFF
+	#
+	# 	self._set_duty((r>>3)*self.intensity, 2)
+	# 	self._set_duty((g>>3)*self.intensity, 1)
+	# 	self._set_duty((b>>3)*self.intensity, 0)
+
+
+	def _set_duty(self, duty, diode):
+		assert diode in range(3)
+		assert 0 <= duty <= 0x0fff
+		duty_ = 0x0fff - duty
+
+		self.controller.write_to_register(self.controller.LEDX_YY_Z(diode, 0, 1), (duty  >> 8)  & 0x0f)
+		self.controller.write_to_register(self.controller.LEDX_YY_Z(diode, 1, 1), (duty_ >> 8)  & 0x0f)
+		self.controller.write_to_register(self.controller.LEDX_YY_Z(diode, 0, 0), duty  & 0x00ff)
+		self.controller.write_to_register(self.controller.LEDX_YY_Z(diode, 1, 0), duty_ & 0x00ff)
+
+
+if __name__ == "__main__":
+	L = ServoDriver()
